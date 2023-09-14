9d9d70c394f1de1e1251472a1e07f8c828a51e5f
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-14 15:35:35 -0300

    Move `shoulda-matchers` to outside of Rspec settings

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
-  has_many :memberships, inverse_of: :team
+  has_many :memberships, inverse_of: :team, dependent: :destroy
   has_many :users, through: :memberships
   has_many :roles, through: :memberships
 
