a996bff0ca5142490cd0e3e3a1cbb04caf061959
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-14 18:36:53 -0300

    Add pre-defined roles
    Add pre-defined roles:
    - Developer
    - Product Owner
    - Tester

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Create roles
+roles = %w[Developer Product Owner Tester]
+
+roles.each do |role_name|
+  Role.find_or_create_by(name: role_name)
+end
