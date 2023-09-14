924d8dcb439be46bdd2451fcb0e796aab1f93b79
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-14 15:17:00 -0300

    Add `roles` specs

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:memberships).inverse_of(:role) }
+  end
 
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
