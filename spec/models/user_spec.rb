32acde186674add6e7873b5430015d4b01388638
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-14 15:23:49 -0300

    Add `user` specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:memberships).inverse_of(:user) }
+    it { is_expected.to have_many(:teams).through(:memberships) }
+    it { is_expected.to have_many(:roles).through(:memberships) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_presence_of(:identifier) }
+  end
 end
