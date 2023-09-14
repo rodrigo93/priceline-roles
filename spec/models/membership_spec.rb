953919739a5e551b7357455577ab6b9f795e9d6a
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-14 15:23:41 -0300

    Add `membership` specs

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Membership, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:user).inverse_of(:memberships) }
+    it { is_expected.to belong_to(:team).inverse_of(:memberships) }
+    it { is_expected.to belong_to(:role).inverse_of(:memberships) }
+  end
 end
