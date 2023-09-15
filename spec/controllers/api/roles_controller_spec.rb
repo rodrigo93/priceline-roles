d63bf2eec8fa17b8193396706c22e44373aa2845
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-15 16:04:10 -0300

    Add Roles#memberships action
    Allow look up memberships for a given role

--- spec/controllers/api/roles_controller_spec.rb
+++ spec/controllers/api/roles_controller_spec.rb
@@ -3,6 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Api::RolesController, type: :controller do
+  shared_examples 'returning not found' do
+    it do
+      subject
+
+      expect(response).to have_http_status(:not_found)
+      expect(response.parsed_body['error']).to match(/Couldn't find Role with/)
+    end
+  end
+
   describe 'GET /index' do
     subject { get :index, params: { page:, per: }, format: :json }
 
@@ -19,7 +28,7 @@ RSpec.describe Api::RolesController, type: :controller do
     end
 
     context 'when there are less than `per` amount of roles' do
-      let!(:roles) { FactoryBot.create_list(:role, 2) }
+      let!(:roles) { create_list(:role, 2) }
       let(:page)   { 1 }
       let(:per)    { 3 }
 
@@ -32,7 +41,7 @@ RSpec.describe Api::RolesController, type: :controller do
     end
 
     context 'when there are more than `per` amount of roles' do
-      let!(:roles) { FactoryBot.create_list(:role, 3) }
+      let!(:roles) { create_list(:role, 3) }
       let(:page)   { 1 }
       let(:per)    { 2 }
 
@@ -52,7 +61,7 @@ RSpec.describe Api::RolesController, type: :controller do
     end
 
     context 'when pagination params are not specified' do
-      let!(:roles) { FactoryBot.create_list(:role, 3) }
+      let!(:roles) { create_list(:role, 3) }
       let(:page)   { nil }
       let(:per)    { nil }
 
@@ -65,7 +74,7 @@ RSpec.describe Api::RolesController, type: :controller do
     end
 
     context 'when pagination params are of wrong type' do
-      let!(:roles) { FactoryBot.create_list(:role, 2) }
+      let!(:roles) { create_list(:role, 2) }
       let(:page)   { 'asdsad' }
       let(:per)    { 'aaaaaa' }
 
@@ -108,7 +117,7 @@ RSpec.describe Api::RolesController, type: :controller do
     subject { get :show, params: role_params, format: :json }
 
     context 'when the role exists' do
-      let(:role) { FactoryBot.create(:role) }
+      let(:role) { create(:role) }
       let(:role_params) do
         { id: role.id }
       end
@@ -122,18 +131,62 @@ RSpec.describe Api::RolesController, type: :controller do
     end
 
     context 'when the role does not exist' do
+      let(:role_params) { { id: 999 } }
+
+      it_behaves_like 'returning not found'
+    end
+  end
+
+  describe 'GET /memberships' do
+    subject { get :memberships, params: role_params, format: :json }
+
+    context 'when the role exists' do
+      let(:role) { create(:role) }
       let(:role_params) do
-        { id: 999 }
+        { id: role.id }
       end
 
-      it 'returns not found with an error message' do
-        subject
+      context 'with no memberships' do
+        it 'returns an empty list' do
+          subject
 
-        expect(response).to have_http_status(:not_found)
-        expect(response.parsed_body).to eq(
-          { 'error' => "Couldn't find Role with 'id'=#{role_params[:id]}" }
-        )
+          expect(response).to have_http_status(:ok)
+          expect(response.parsed_body).to be_empty
+        end
       end
+
+      context 'with memberships' do
+        let!(:memberships) { create_list(:membership, 2, role:) }
+
+        it 'returns a list of memberships' do
+          subject
+
+          expect(response).to have_http_status(:ok)
+          expect(response.parsed_body.size).to eq(2)
+        end
+      end
+
+      context 'with pagination' do
+        let!(:memberships) { create_list(:membership, 2, role:) }
+        let(:page) { 1 }
+        let(:per) { 1 }
+        let(:role_params) do
+          { id: role.id, page:, per: }
+        end
+
+        it 'returns a paginated list of memberships' do
+          subject
+
+          expect(response).to have_http_status(:ok)
+          expect(response.parsed_body.size).to eq(per)
+        end
+      end
+    end
+
+    context 'when the role does not exist' do
+      let(:role_params) { { id: 999 } }
+
+      it_behaves_like 'returning not found'
     end
   end
 end
