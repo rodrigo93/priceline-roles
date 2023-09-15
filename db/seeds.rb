54d3adc6210fc453980e7273e328ca0b14ef4d26
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-09-15 18:47:43 -0300

    Fix seeds
    It was creating 4 roles instead of 3
    as Product Owner were 2 separate
    roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 # Create roles
-roles = %w[Developer Product Owner Tester]
+roles = ['Developer', 'Product Owner', 'Tester']
 
 roles.each do |role_name|
   Role.find_or_create_by(name: role_name)
